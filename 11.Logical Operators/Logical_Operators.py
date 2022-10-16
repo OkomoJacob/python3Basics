6bd2a0ad122894c1f628467463d6b769aac8fc6e
okomojacob <jaylabs2030@gmail.com>
2022-10-16 10:37:12 +0300

    :sparkles: Getting started :tada: with handling logical operations :books:, :pencil2:

---
+++ 11.Logical Operators/Logical_Operators.py
@@ -0,0 +1,8 @@
+# Check if applicant has high income and good credit
+#    Eligible for loan
+_has_criminal_record = False
+_has_high_income = True
+_has_good_credit = False
+
+if _has_high_income or _has_good_credit and not _has_criminal_record:
+    print("Eligible for Loan")
