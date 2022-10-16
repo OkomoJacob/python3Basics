d946edb4b922303ad299d289146db0da30bdae82
okomojacob <jaylabs2030@gmail.com>
2022-10-16 11:15:27 +0300

    :tada: Playing around with :hammer: Data types :books:, :pencil2:

---
+++ 17.Tuples/16_b_unpacking.py
@@ -0,0 +1,6 @@
+# Using commas, it takes the first coordinate and assignss it  x, 3nd to y an d3rd to z
+# Usefull to both lists
+coordinates = (1, 2, 3)
+
+x, y, z = coordinates
+print(y)
---
+++ 17.Tuples/17_a_tuples.py
@@ -0,0 +1,8 @@
+# Similar to lists but are immutable: Cannot edit
+
+numbers = (1, 2, 3)
+print(numbers)
+'''numbers [0] = 4
+print(numbers[0]) '''#TypeError: 'tuple' object does not support item assignment
+'''__ Double underscores are called magic methods are are way advanced and go abeyond'''
+
