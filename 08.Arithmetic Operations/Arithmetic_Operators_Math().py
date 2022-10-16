c6a3ce4799bbc17cc3f3839039c8b467a951ef9b
okomojacob <jaylabs2030@gmail.com>
2022-10-16 10:33:30 +0300

    :sparkles: Getting started :tada: with handling Math operations :books:, :pencil2:

---
+++ 08.Arithmetic Operations/Arithmetic_Operators_Math().py
@@ -0,0 +1,23 @@
+# / this returns the decimal e.g 20 / 3 = 6.666666666666667
+# // this returns the whole number quotient e.g 20 / 3 = 6
+# ** this returns the exponential e.g 20 ** 3 = 8000
+# % this returns the remainder e.g 20 % 3 = 2
+
+a = 20
+b = 3
+print(a / b)
+print(a // b)
+print(a ** b)
+print(a % b)
+
+# Argumented assignment operators
+c = 5
+c += 3
+print(c)
+
+# MATH FUNCTIONS
+d = -2.5674
+print(abs(round(d)))
+
+
+
