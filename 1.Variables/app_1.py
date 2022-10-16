0cdbfe602bb08ac5fdbe1b57b6c6b56242fec85d
okomojacob <jaylabs2030@gmail.com>
2022-10-16 10:13:50 +0300

    :sparkles: Getting started :tada: with Variable :books:, :pencil2:

---
+++ 1.Variables/app_1.py
@@ -0,0 +1,18 @@
+# integer
+price = 10
+# float
+rating = 4.9
+# boolean
+is_published = False
+is_published = True
+
+# Data types
+# string
+# name = 'John Smith'
+# int
+age = 20
+# boolean
+is_new = True
+# name in this case is the variable where the input will be stored
+
+
