d68acdcaecd45d83e3bdfa3c2a0b40e92f00a8b9
okomojacob <jaylabs2030@gmail.com>
2022-10-16 11:18:18 +0300

    :tada: Error handling :bug: :hammer:  :books:, :pencil2:

---
+++ 20.Exceptions/errors_handle.py
@@ -0,0 +1,20 @@
+'''
+Try running the code and anticipate for the errors and
+ when a ValueError is encountered, catch it with an exception message properly
+'''
+try:
+    age = int(input('Enter age: '))
+    print(age)
+except ValueError: #Catch the error
+    print('Invalid input, Integers only')
+
+
+
+try:
+    age = int(input('Enter age: '))
+    income = 300
+    risk = income / age
+    
+except ZeroDivisionError: #Catch the error
+    print('Invalid input, Cannot divide number by Zero')
+
